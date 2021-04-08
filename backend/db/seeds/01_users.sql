84d389b84d287c1a54e5114d411b09f487fc9227
Kevin Phan <31554101+dattphan15@users.noreply.github.com>
2021-04-08 15:52:07 -0700

    Features/seed data (#16)
    * initial seed data
    
    * test seed data

---
+++ backend/db/seeds/01_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (first_name, last_name, email, password)
+VALUES
+('Kevin', 'Phan', 'dattphan5@gmail.com', 'password'),
+('Connie', 'Ho', 'connie_ho@gmail.com', 'password'),
+('Justin', 'Ly', 'jl.justin15@gmail.com', 'password');
---
+++ backend/db/seeds/02_coffees.sql
@@ -0,0 +1,50 @@
+INSERT INTO coffees (name, description, region, roast, brand, acidity, type, grain_species, image_url)
+VALUES
+
+('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+Certified organic coffee is grown free from harmful chemicals and pesticides
+Fair trade certification ensures farmers and their families receive a fair price for their coffee
+A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
+Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'http://cdn.shopify.com/s/files/1/0059/2772/products/lush-wb_grande.jpg?v=1594355879'), 
+
+('Ethical Bean Fair Trade Organic Coffee Lush', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+Certified organic coffee is grown free from harmful chemicals and pesticides
+Fair trade certification ensures farmers and their families receive a fair price for their coffee
+A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
+Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://cdn.shopify.com/s/files/1/0059/2772/products/lush-wb_600x600.jpg?v=1594355879'), 
+
+('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+Certified organic coffee is grown free from harmful chemicals and pesticides
+Fair trade certification ensures farmers and their families receive a fair price for their coffee
+A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
+Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://cdn.shopify.com/s/files/1/0059/2772/products/sweet-wb_600x600.jpg?v=1594355881'), 
+
+('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+Certified organic coffee is grown free from harmful chemicals and pesticides
+Fair trade certification ensures farmers and their families receive a fair price for their coffee
+A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
+Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://cdn.shopify.com/s/files/1/0059/2772/products/superdark-wb_600x600.jpg?v=1594355886'), 
+
+('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+Certified organic coffee is grown free from harmful chemicals and pesticides
+Fair trade certification ensures farmers and their families receive a fair price for their coffee
+A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
+Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://shop.cdn.ethicalbean.com/products/bold-dark-roast-organic-coffee-beans'), 
+
+('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+Certified organic coffee is grown free from harmful chemicals and pesticides
+Fair trade certification ensures farmers and their families receive a fair price for their coffee
+A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
+Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://shop.cdn.ethicalbean.com/products/chemical-free-water-process-decaf-dark-roast-whole-bean-coffee'), 
+
+('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+Certified organic coffee is grown free from harmful chemicals and pesticides
+Fair trade certification ensures farmers and their families receive a fair price for their coffee
+A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
+Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://cdn.shopify.com/s/files/1/0059/2772/products/classic-wb_600x600.jpg?v=1594355890'), 
+
+('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+Certified organic coffee is grown free from harmful chemicals and pesticides
+Fair trade certification ensures farmers and their families receive a fair price for their coffee
+A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
+Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://cdn.shopify.com/s/files/1/0059/2772/products/exotic-wb_600x600.jpg?v=1594355883');
