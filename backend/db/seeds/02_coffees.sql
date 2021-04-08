8674d6dacad9de1ab91f45d70eeb86d851912450
Justin Ly <jly3@ualberta.ca>
2021-04-08 17:48:14 -0600

    added coffees seeds

--- backend/db/seeds/02_coffees.sql
+++ backend/db/seeds/02_coffees.sql
@@ -1,4 +1,4 @@
-INSERT INTO coffees (name, description, region, roast, brand, acidity, type, grain_species, image_url)
+INSERT INTO coffees (name, description, region, roast, brand, acidity, grain_species, image_url)
 VALUES
 
 ('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
@@ -19,7 +19,7 @@ Fair trade certification ensures farmers and their families receive a fair price
 A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
 Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://cdn.shopify.com/s/files/1/0059/2772/products/sweet-wb_600x600.jpg?v=1594355881'), 
 
-('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag of Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
+('Ethical Bean Fair Trade Organic Coffee', 'One 907g (2lb) bag o, 'https://cdn.shopify.com/s/files/1/0059/2772/products/exotic-wb_600x600.jpg?v=1594355883');f Ethical Bean Fair Trade Organic Lush Medium Dark Roast whole bean coffee
 Certified organic coffee is grown free from harmful chemicals and pesticides
 Fair trade certification ensures farmers and their families receive a fair price for their coffee
 A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
@@ -48,3 +48,58 @@ Certified organic coffee is grown free from harmful chemicals and pesticides
 Fair trade certification ensures farmers and their families receive a fair price for their coffee
 A classic medium roast with elegant acidity highlighting a nuanced, medium bodied cup
 Resealable bag with pull-ab zip closure for long-lasting freshness', 'Guatemala', 'Dark Roast', 'Ethical Bean Coffee', 'Medium to High', 'Blend', 'Arabica', 'https://cdn.shopify.com/s/files/1/0059/2772/products/exotic-wb_600x600.jpg?v=1594355883');
+
+('Alasitas Java Coco Natural - Espresso', 
+'The Java variety originates from Ethiopia and has a wonderful and complex flavour profile that is super unique to its variety. The Rodriguez family produces beautiful and delicious Javas and we are so happy to have these coffees in our collection.', 
+'Yungas', 
+'Dark', 
+'Monogram Coffee', 
+'Low', 
+'Robusta', 
+'https://cdn.shopify.com/s/files/1/0702/0489/products/ALASITAS_Espresso_352x544.png?v=1614378824'),
+
+('Desta Gola - Espresso',
+ 'Desta Gola is part of Royal Coffee’s “Single Farmer Lots Program”, a program designed to separate single farmer lots from the larger cooperative blends sold through the Ethiopian Commodity Exchange. Through yearly visits and regular communication, Royal works with the farmers to ensure they follow strict farm management and post-harvest protocols. This has resulted in increasing cup quality and higher returns for these producers year after year.',
+  'Ethiopia',
+  'Dark',
+  'Monogram Coffee',
+  'Medium',
+  'Robusta',
+  'https://cdn.shopify.com/s/files/1/0702/0489/products/DESTAGOLA_Espresso1_576x896.png?v=1616523601'),
+
+('Miguel Angel Ruiz Pacas',
+ "Miguel was one of the first producers we ever bought from when we first started roasting. He grows both Parainema and Pacas varieties on his farm, located in La Leona, a micro region of Santa Barbara. The Pacas variety is a natural spontaneous mutation of the Bourbon variety. It is very hardy, can thrive in many different environments and produces good quality in the cup. Miguel's coffees are always intense and unique, which make this really exciting!",
+ "Honduras",
+ "Medium",
+ "Monogram Coffee",
+ "Low",
+ "Arabica",
+ "https://cdn.shopify.com/s/files/1/0702/0489/products/MIGUEL_Filter_pacas_352x576.png?v=1614817745"),
+
+("Alasitas Java Mocha",
+  "The Java variety originates from Ethiopia and has a wonderful and complex flavour profile that is super unique to its variety. The Rodriguez family produces beautiful and delicious Javas and we are so happy to have these coffees in our collection.",
+  "Bolivia",
+  "Dark",
+  "Monogram Coffee",
+  "Medium-High",
+  "Arabica",
+  "https://cdn.shopify.com/s/files/1/0702/0489/products/ALASITAS_Filter_576x928.png?v=1614284629"),
+
+("God Father Espresso", 
+"Smooth and Rich, Notes of Milk Chocolate and Caramel. This is a rock solid coffee which preforms well as straight espresso, as well as in larger 16oz milk drinks.",
+"Brazil",
+"Dark",
+"Analog Coffee",
+"High",
+"Robusta",
+"https://cdn.shopify.com/s/files/1/1521/8648/products/godfathernewwhite_grande.jpg?v=1563287995"),
+
+("Mambo #5",
+ "Delicate and lush, with notes of red wine and mixed berry. This java originates from the El Salvadar and is a local favorite."
+ "El Salvador",
+ "Light",
+ "Analog Coffee",
+ "Medium-High",
+ "Arabica",
+ "https://cdn.shopify.com/s/files/1/1521/8648/products/Mambo5_grande.jpg?v=1602599464")
+
