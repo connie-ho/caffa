47e193df22a76480f1f7caf3a407f123d3382f2c
Connie Ho <hconnie09@gmail.com>
2021-04-08 19:24:51 -0600

    add comma

--- backend/db/seeds/04_reviews.sql
+++ backend/db/seeds/04_reviews.sql
@@ -6,7 +6,7 @@ VALUES
 (5, 'Really like this coffee, smooth and elegant!', 3, 2),
 (5, 'Amazing coffee, really great for the mornings',3, 3),
 (2, 'Didnt like this one so much...', 3, 5),
-(3, 'Great coffee :)', 3, 4)
+(3, 'Great coffee :)', 3, 4),
 (1, 'Actually kicked my ass, do not recommend', 2, 3),
 (3, 'Just enough ass kicking!!', 2, 4),
 (1, 'Save your money and buy real beans', 2, 5),
