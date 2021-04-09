8afd32d8f376a8aa58524f0520e1cee2610a469b
Kevin Phan <ken.phan@live.ca>
2021-04-09 07:29:44 +0000

    setup routers for users and favourites, helpers, changes to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,45 @@
+// load .env data into process.env
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+require('dotenv').config();
+const dbHelpers = require('./helpers/dbHelpers')(db);
+
+const PORT       = process.env.PORT || 8080;
+const ENV        = process.env.ENV || "development";
+const cookieSession = require('cookie-session');
 
 const app = express();
-
 const db = require("./db");
+db.connect();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const coffeesRouter = require("./routes/coffees");
+const favouritesRouter = require("./routes/favourites");
+const reviewsRouter = require("./routes/reviews");
+
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/coffees", coffeesRouter);
+app.use("/api/favourites", favouritesRouter);
+app.use("/api/reviews", reviewsRouter);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,7 +47,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/api/users', usersRouter(dbHelpers));
+
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
 
 module.exports = app;
