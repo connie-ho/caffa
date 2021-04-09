7ed30d9701e2c271e446bfc03bd45a586de9b796
Justin Ly <jly3@ualberta.ca>
2021-04-08 19:21:58 -0600

    changed user id

--- backend/db/seeds/04_reviews.sql
+++ backend/db/seeds/04_reviews.sql
@@ -3,7 +3,7 @@ VALUES
 (4, 'Fantastic coffee highly recommended to anyone who loves to drink coffee throughout the day', 1, 1),
 (4, 'Love it!', 1, 2),
 (4, 'Really surprised how this coffee turned out. Would purchase this item again.', 1, 3),
-(5, 'Really like this coffee, smooth and elegant!', 2, 2),
-(5, 'Amazing coffee, really great for the mornings',2, 3),
-(2, 'Didnt like this one so much...', 2, 5),
-(3, 'Great coffee :)', 2, 4)
+(5, 'Really like this coffee, smooth and elegant!', 3, 2),
+(5, 'Amazing coffee, really great for the mornings',3, 3),
+(2, 'Didnt like this one so much...', 3, 5),
+(3, 'Great coffee :)', 3, 4)
