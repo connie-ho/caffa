a8ab91a1d359bf6635757d759801fbbddb943d8a
Connie Ho <hconnie09@gmail.com>
2021-04-10 12:44:41 -0600

    add pagination functionality

--- frontend/src/components/coffees/CoffeeList.jsx
+++ frontend/src/components/coffees/CoffeeList.jsx
@@ -1,9 +1,10 @@
 import {Route, Switch} from 'react-router-dom';
-import {useContext} from 'react';
+import {useContext,useState} from 'react';
 
 import CoffeeContext from '../../contexts/CoffeeContext';
 import ReviewContext from '../../contexts/ReviewContext';
 import CoffeeListItem from './CoffeeListItem';
+import Pagination from './Pagination';
 
 import {getReviewsForCoffee, avgRatingForCoffee} from '../../helpers/selectors';
 
@@ -11,8 +12,21 @@ const CoffeeList = (props) => {
   const {coffees} = useContext(CoffeeContext);
   const {reviews} = useContext(ReviewContext);
 
-  const coffeeList = coffees.map(coffee => {
+  
+  // Pagination Logic
+  const[currentPage, setCurrentPage] = useState(1);
+  const[coffeesPerPage, setCoffeesPerPage] = useState(6);
+  
+  const indexOfLastCoffee = currentPage * coffeesPerPage;
+  const indexOfFirstCoffee = indexOfLastCoffee - coffeesPerPage;
+  const currentCoffees = coffees.slice(indexOfFirstCoffee, indexOfLastCoffee)
+  
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+
+  // Create Coffee List Item
+  const coffeeList = currentCoffees.map(coffee => {
     const coffeeReviews = getReviewsForCoffee(reviews,coffee.id)
     const avgRating = avgRatingForCoffee(coffeeReviews);
 
@@ -36,14 +50,11 @@ const CoffeeList = (props) => {
         <Route path="/coffees">
           <h1>All Coffees</h1>
           {coffeeList}
-         {/* {addCoffeeArray.map((coffee) => {
-           return (
-             <CoffeeListItem 
-             key={coffee.id}
-             coffee={coffee} 
-             />
-           )
-         })} */}
+          <Pagination 
+            coffeesPerPage={coffeesPerPage}
+            totalCoffees={coffees.length}
+            paginate={paginate}
+          />
         </Route>
       </Switch>
     </div>
