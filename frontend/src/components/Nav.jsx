2b2a12a653c739c5419f13728baaac70b2976f41
Justin Ly <jly3@ualberta.ca>
2021-04-14 21:36:44 -0600

    added nav bar spacer, and reduced text size

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,6 +3,7 @@ import UserContext from '../contexts/UserContext';
 import {Link} from 'react-router-dom';
 
 import { fade, makeStyles } from '@material-ui/core/styles';
+import { Icon } from "@material-ui/core"
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -18,7 +19,8 @@ import CameraAltIcon from '@material-ui/icons/CameraAlt';
 import FormDialog from './image-search/Dialog';
 import SearchBar from './dynamic-search/SearchBar';
 import Autocomplete from "@material-ui/lab/Autocomplete";
-
+import headIcon from "../images/Caffa2.png"
+import Paper from '@material-ui/core/Paper'
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -84,6 +86,7 @@ const useStyles = makeStyles((theme) => ({
       display: 'none',
     },
   },
+  toolbar: theme.mixins.toolbar
 }));
 
 
@@ -191,14 +194,16 @@ export default function Nav(props) {
 
         <Toolbar>
           <Link to="/">
-            <IconButton
+            <Icon
               edge="start"
               className={classes.menuButton}
               color="inherit"
               aria-label="open drawer"
+              size='small'
               >
-              <LocalCafeIcon />
-            </IconButton>
+
+              <img src={headIcon} width="60" height="50"  /> 
+            </Icon>
           </Link>
           <Link to="/coffees">
             <Typography className={classes.title} variant="h6" noWrap>
@@ -269,6 +274,9 @@ export default function Nav(props) {
       {renderMobileMenu}
       {renderMenu}
       {renderModal}
+      <Paper>
+        <div className={classes.toolbar} />
+      </Paper>
     </div>
   );
 }
