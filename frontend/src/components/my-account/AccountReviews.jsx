40f125d3ee18553992506ca666afaf1ef28c27f1
dattphan15 <ken.phan@live.ca>
2021-04-17 16:05:53 -0700

    fix hardcoded user id for account reviews

--- frontend/src/components/my-account/AccountReviews.jsx
+++ frontend/src/components/my-account/AccountReviews.jsx
@@ -1,4 +1,5 @@
-import {useContext} from 'react';
+import { useState, useContext, useEffect } from 'react';
+import UserContext from '../../contexts/UserContext';
 import ReviewListItem from '../coffee-id/ReviewListItem';
 import DataContext from '../../contexts/DataContext';
 import {getUserReviews} from '../../helpers/selectors';
@@ -8,12 +9,30 @@ import classes from '../coffee-id/Coffee.module.scss';
 function AccountReviews(props) {
   const {coffee} = props;
   console.log("accountreview coffee: ", props)
+  const {user} = useContext(UserContext);
   const {state} = useContext(DataContext);
   const reviews = state.reviews;
   const users = state.users;
 
+  const [values, setValues] = useState({
+    id: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+  })
 
-  const coffeeReviews = getUserReviews(Object.values(reviews), 1).sort((a,b)=> {return new Date(b.created_at) - new Date(a.created_at)});
+  useEffect(()=>{
+    setValues(prev => ({
+      ...prev,
+      id: user? user.id : '',
+      first_name: user? user.first_name : '',
+      last_name: user? user.last_name : '',
+      email: user? user.email : '',
+    }))
+  },[user])
+
+
+  const coffeeReviews = getUserReviews(Object.values(reviews), values.id).sort((a,b)=> {return new Date(b.created_at) - new Date(a.created_at)});
 
   console.log("coffee reviews :", coffeeReviews)
 
