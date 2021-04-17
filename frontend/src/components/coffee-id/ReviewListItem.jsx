1e92b6a831991c61c87206cf243cf0f0dc7d4fad
Connie Ho <hconnie09@gmail.com>
2021-04-17 12:24:35 -0600

    style review and review form

--- frontend/src/components/coffee-id/ReviewListItem.jsx
+++ frontend/src/components/coffee-id/ReviewListItem.jsx
@@ -14,29 +14,54 @@ import Typography from '@material-ui/core/Typography';
 
 import bean from '../../images/31080.png';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme=>({
   root: {
-    minWidth: 400,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: '1em',
+    maxWidth: '800px',
     border: '0px solid',
     borderRadius: '1rem',
     fontSize: '1.25em',
-    marginBottom: '1em'
+    marginBottom: '2em',
+    background: 'rgb(255,247,245)',
+    "&:hover": { 
+      transform: "scale3d(1.05, 1.05, 1)", 
+      background: '#e4d3cf'},
   },
   image:{
     width: '2em',
     height: '2em',
     border: '0px solid',
     borderRadius: '50%',
-    marginRight: '0.5em'
+    marginRight: '0.75em'
   },
   userInfo:{
     display: 'flex',
     alignItems: 'center',
+    marginTop: '0.5em',
     fontWeight: 600
   },
+  rating: {
+    background: theme.palette.primary.main,
+    border: '1px solid #FCE4A2',
+    borderRadius: '0.5em',
+    padding: '0.2em 0.5em',
+    marginRight: "1em",
+  },
+  bean: {
+    marginLeft: "1em"
+  },
+  button: {
+    marginRight: "1em"
+  },
+  description: {
+    marginTop: '0.5em',
+    wordWrap: 'break-word'
+  }
 
-
-});
+})));
 
 export default function ReviewListItem(props) {
 
@@ -74,12 +99,12 @@ export default function ReviewListItem(props) {
     <>
     <Card className={classes.root}>
       <CardContent>
-       <div>
-         {review.rating} 
-        <img src={bean} style={{ height:'15px', width:'15px'}}/> 
-       </div>
-       <div>
-         {review.description}
+         <span className={classes.rating}>
+          {review.rating} 
+          <img src={bean} style={{ height:'15px', width:'15px', marginLeft: '0.1em'}}/> 
+        </span>
+       <div className={classes.description}>
+        {review.description}
        </div>
        <div className={classes.userInfo}>
         <img 
@@ -99,6 +124,7 @@ export default function ReviewListItem(props) {
           (user && user.id === review.user_id) ? 
           (<div>
             <Button
+              className={classes.button}
               variant="outlined"
               color="primary" 
               size="small"
