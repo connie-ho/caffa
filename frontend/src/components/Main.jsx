f2d51b92aaa09a195be80f09413699e97f6803d0
dattphan15 <ken.phan@live.ca>
2021-04-17 22:00:52 -0700

    snapshot of footer issue after separating Account Drawer component

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {useState, useContext} from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Grid } from "@material-ui/core";
 import Nav from './Nav';
 import Login from './Login';
@@ -14,9 +14,45 @@ import HomeContext from '../contexts/HomeContext';
 import Account from './Account';
 import AccountFavourites from './my-account/AccountFavourites';
 import AccountSettings from './my-account/AccountSettings';
+import AccountProfile from './my-account/AccountProfile';
+import AccountReviews from './my-account/AccountReviews';
+import AccountDrawer from './my-account/AccountDrawer';
 import Register from './Register'
 import Footer from './Footer'
+import { makeStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 240;
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '5rem 20rem',
+    display: 'block',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    boxSizing: 'border-box',
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+
 const Main = (props) => {
+
+  const classes = useStyles();
 
   const {
     addFavourite, 
@@ -41,7 +77,27 @@ const Main = (props) => {
     }
   )
 
+  const [openReviewForm, setOpenReviewForm] = useState(false);
+  const [values, setValues] = useState({
+    id: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+  })
+
+  useEffect(()=>{
+    setValues(prev => ({
+      ...prev,
+      id: user? user.id : '',
+      first_name: user? user.first_name : '',
+      last_name: user? user.last_name : '',
+      email: user? user.email : '',
+    }))
+  },[user])
+  
+
   const {state} = useContext(DataContext);
+  const coffees = state.coffees;
   
 
   return (
@@ -70,15 +126,38 @@ const Main = (props) => {
               </FavouriteContext.Provider>
             </ReviewContext.Provider>
           </Route>
-          <Route path="/account">
-          <ReviewContext.Provider value={{addReview, editReview, deleteReview}}>
 
-            <Account 
-              editUserHandler={editUserHandler}
-            />
-            </ReviewContext.Provider>
+      <ReviewContext.Provider value={{addReview, editReview, deleteReview}}>
+        <Route path="/account/favourites">
+          <AccountDrawer />
+          <AccountFavourites 
+            limit={0}
+          />
+        </Route>
+        <Route path="/account/reviews">
+          <AccountDrawer />
+          <AccountReviews
+            user={user}
+            coffee={coffees}
+            openReviewForm={openReviewForm}
+            setOpenReviewForm={setOpenReviewForm}
+          />
+        </Route>
+        <Route path="/account/settings">
+          <AccountDrawer />
+          <AccountSettings
+            classes={classes}
+          />
+        </Route>
+        <Route path="/account" exact>
+          <AccountDrawer />
+          <AccountProfile />
+          <AccountFavourites
+            limit={3}
+          />
+        </Route>
+        </ReviewContext.Provider>
 
-          </Route>
           <Route path="/login" >
             <Login/>
           </Route>
