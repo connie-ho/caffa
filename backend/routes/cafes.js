eedac2ce9e4d9f8f779161f2b5477bb7bca68a19
Connie Ho <hconnie09@gmail.com>
2021-04-17 18:51:24 -0600

    get rid of console log

--- backend/routes/cafes.js
+++ backend/routes/cafes.js
@@ -6,9 +6,9 @@ const {getCafeData} = require('../db/helpers/yelp-queries');
 
 router.get("/", (req, res) => {
 
-  console.log(req.params.query)
+  const query = req.query.brand.split("+").join(" ");
 
-  getCafeData(req.params.query)
+  getCafeData(query)
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
