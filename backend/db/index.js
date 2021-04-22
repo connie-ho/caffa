1bc2ded12152c0c850f7e5b3323a80f90fa47403
Justin Ly <jly3@ualberta.ca>
2021-04-22 13:56:45 -0600

    heroku testing v4

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 
 const client = new pg.Client({
+  ssl:{rejectUnauthorized: false},
   connectionString: process.env.DATABASE_URL || connectionString ,
 });
 
