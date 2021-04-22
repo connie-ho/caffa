f5c8ca216ed79c832e2ed5a805604a90d66cfdf0
Justin Ly <jly3@ualberta.ca>
2021-04-22 13:05:39 -0600

    heroku testing

--- backend/app.js
+++ backend/app.js
@@ -72,7 +72,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(
   cookieSession({
     name: "session",
