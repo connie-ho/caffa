2306c9f73a4c945d66a5667a098a024f00d1482e
Connie Ho <hconnie09@gmail.com>
2021-04-22 11:05:13 -0600

    change bcrypt

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // use bcrypt to hash passwords
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const {
   getUserById,
