5af343bc64b22362cea98811179a12172edde722
Connie Ho <hconnie09@gmail.com>
2021-04-22 13:43:23 -0600

    change connection string

--- backend/app.js
+++ backend/app.js
@@ -20,13 +20,6 @@ const db = require("./db");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use("/styles", sass({
-//   src: __dirname + "/styles",
-//   dest: __dirname + "/public/styles",
-//   debug: true,
-//   outputStyle: 'expanded'
-// }));
-app.use(express.static("public"));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1']
--- backend/db/index.js
+++ backend/db/index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 
 const client = new pg.Client({
-  connectionString: connectionString || process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || connectionString,
   ssl: { rejectUnauthorized: false }
 });
 
